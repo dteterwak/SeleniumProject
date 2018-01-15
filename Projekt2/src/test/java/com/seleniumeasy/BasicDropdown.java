f1047f63e66383b2a8ddc28b0475a91d30f1f86b
dteterwak <32641112+dteterwak@users.noreply.github.com>
2018-01-15 15:42:44 +0100

    Updated BasicDropdown

--- Projekt2/src/test/java/com/seleniumeasy/BasicDropdown.java
+++ Projekt2/src/test/java/com/seleniumeasy/BasicDropdown.java
@@ -3,8 +3,10 @@ package com.seleniumeasy;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
@@ -30,12 +32,17 @@ public class BasicDropdown {
     }
 
     public void multipleSelect(){
-        WebElement element = driver.findElement(By.id("multi-select"));
+        /*WebElement element = driver.findElement(By.id("multi-select"));
         Select se = new Select(element);
+        se.isMultiple();
+
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.LEFT_CONTROL).build().perform();
 
         se.selectByValue("Texas");
         se.selectByValue("New Jersey");
         se.selectByValue("California");
+        actions.keyUp(Keys.LEFT_CONTROL).build().perform();
 
         List<WebElement> allSelectedOptions = se.getAllSelectedOptions();
         for (WebElement webElement : allSelectedOptions)
@@ -45,6 +52,44 @@ public class BasicDropdown {
 
         driver.findElement(By.xpath("//button[@id='printAll']")).click();
 
+*/
+
+        String[] stateOptions = {"Texas", "California", "New Jersey"};
+        WebElement element = driver.findElement(By.id("multi-select"));
+        Select dropdown = new Select(element);
+        List<WebElement> options = dropdown.getOptions();
+        Actions builder = new Actions(driver);
+        boolean isMultple = dropdown.isMultiple();
+        if(isMultple){
+            dropdown.deselectAll();
+        }
+        builder.keyDown(Keys.LEFT_CONTROL);
+        for(String stateOption : stateOptions){
+            for(WebElement option : options){
+                String optionText = option.getText().trim();
+                if(optionText.equalsIgnoreCase(stateOption)){
+                    if(isMultple){
+                        if(!option.isSelected()){
+                            builder.click(option);
+                        }
+                    } else{
+                        option.click();
+                    }
+                    break;
+                }
+            }
+        }
+        builder.keyUp(Keys.LEFT_CONTROL).build().perform();
+
+        driver.findElement(By.xpath("//button[@id='printMe']")).click();
+
+        try{
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.findElement(By.xpath("//button[@id='printAll']")).click();
 
         try{
             Thread.sleep(2000);
@@ -52,12 +97,11 @@ public class BasicDropdown {
             e.printStackTrace();
         }
 
-
     }
 
     public void assertion(){
         Assert.assertTrue(driver.getPageSource().contains("Day selected :- Wednesday"));
-        Assert.assertTrue(driver.getPageSource().contains("Options selected are : Texas,New Jersey,California"));
+        Assert.assertTrue(driver.getPageSource().contains("Options selected are : Texas,California,New Jersey"));
     }
 
 
